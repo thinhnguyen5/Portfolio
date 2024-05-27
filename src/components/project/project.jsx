6e25092dd8e087cc3a8919cd89e4fb2c669fdc28
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2024-05-27 13:30:50 +0300

    Project

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Img from '../../assets/baber_shop.png';
 import Img2 from '../../assets/keyloop.jpg';
 import Img3 from '../../assets/Img3.png';
+import Img4 from '../../assets/coffee_shop.png';
 import "./project.css";
 
 const Project = () => {
@@ -36,7 +37,7 @@ const Project = () => {
 
           <div className="project__container-item">
             <img className="project__container-item-img" src={Img3} alt="project" />
-            <p>Sep 2022 - Dec 2022</p>
+            <p>Sep 2022-Dec 2022</p>
             <h3>T-shirt e-commerce</h3>
             <p>Make a T-shirt e-commerce web application with frontend and backend part. After everything has been built, deployed to Netlify</p>
             <div className="source">
@@ -46,13 +47,13 @@ const Project = () => {
           </div>
 
           <div className="project__container-item">
-            <img className="project__container-item-img" src={Img2} alt="project" />
-            <p>Sep 2022 - Dec 2022</p>
-            <h3>T-shirt e-commerce</h3>
-            <p>Make a T-shirt e-commerce web application with frontend and backend part. After everything has been built, deployed to Netlify</p>
+            <img className="project__container-item-img" src={Img4} alt="project" />
+            <p>May 2021-Aug 2021</p>
+            <h3>Coffee Shop</h3>
+            <p>A project about a coffee website where there is a coffee menu and addresses of coffee shops in Hanoi Vietnam</p>
             <div className="source">
-              <a href="https://github.com/prod-des-project-2021/group_1" target="_blank" rel="noreferrer">SOURCE CODE</a>
-              <a className='source__deploy' href="" target="_blank" rel="noreferrer">DEPLOY</a>
+              <a href="https://github.com/thinhnguyen5/Summer-Project" target="_blank" rel="noreferrer">SOURCE CODE</a>
+              <a className='source__deploy' href="https://www.youtube.com/watch?v=sMoYKo9IdDk" target="_blank" rel="noreferrer">DEPLOY</a>
             </div>
           </div>
         </div>
